e02568850619fa89b2c7acc9f3ec3dfbf25a8d3d
Hariel <hariel96@gmail.com>
2020-03-17 20:40:52 -0300

    Subindo os arquivos

---
+++ src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// o objeto file ira encapsular todo processo de acessar o arquivo e caminho
+		File file = new File("C:\\Users\\re039866\\Desktop\\test.txt"); 
+		Scanner sc = null;
+		//quando instanciar o Scanner a partir do arquivo file o programa tentara abrir o arquivo gerando uma exceção do tipo IOException
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) { // testar se ainda há uma proxima linha no arquivo se sim, imprimi-la
+				System.out.println(sc.nextLine());
+			}
+		}
+		catch (IOException e){
+			System.out.println("Error: " + e.getMessage());
+		}
+		finally { // fechamento no finally boa pratica
+			if(sc != null) {
+			sc.close();
+			}
+		}	
+	}
+}
